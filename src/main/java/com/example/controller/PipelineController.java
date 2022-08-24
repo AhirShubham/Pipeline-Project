ffb920498770e2de4fa07554727532a008347dbe
AhirShubham <73291386+AhirShubham@users.noreply.github.com>
2022-08-24 17:03:27 +0530

    Update PipelineController.java

--- src/main/java/com/example/controller/PipelineController.java
+++ src/main/java/com/example/controller/PipelineController.java
@@ -1,12 +1,7 @@
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class PipelineController {
 
-@RequestMapping
 public String sayHello() {
 	return "Hello"; 		
 }
